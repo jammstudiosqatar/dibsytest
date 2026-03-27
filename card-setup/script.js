f6f26ff9299356b63986015a7136f4c170f166fb
jammstudiosqatar <info@jammstudiosqatar.com>
2026-03-27 16:48:52 +0300

    Update script.js

--- card-setup/script.js
+++ card-setup/script.js
@@ -27,7 +27,7 @@ var options = {
 };
 
 // Initialize Dibsy components
-const dibsy = Dibsy("pk_live_Cb1IChUrXLsKSJI51aq1nBhkTENev15SZfqU", {
+const dibsy = Dibsy("pk_test_c28ee081369bedb88409c144b882a23382ce", {
   locale: "en_US",
 });
 var cardNumber = dibsy.createComponent("cardNumber", options);
